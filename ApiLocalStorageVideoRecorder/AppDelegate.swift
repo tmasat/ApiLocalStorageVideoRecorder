424fa70ad7f0c02757f32a8d72fc80ed934d1708
Tugberk Masat <tugberkmasat@yahoo.com>
2023-06-05 06:53:18 +0300

    Fix cameraView is gone when the app enter background bug add popToViewController on sceneDidEnterBackground

--- ApiLocalStorageVideoRecorder/AppDelegate.swift
+++ ApiLocalStorageVideoRecorder/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        /// To test CoreData without deleting the app
 //        let coreDataManager = CoreDataManager()
 //        coreDataManager.deleteAllPlayers()
         
